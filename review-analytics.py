fa6644ee037ec341630d19996df58dd7f9a69817
ken huang <ken.huang@eten.com.tw>
2018-10-12 15:14:11 +0800

    three version

--- review-analytics.py
+++ review-analytics.py
@@ -12,3 +12,23 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('the average string lengh is', sum_len/len(data), 'charactor')
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總共有', len(new), '長度小於100')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('總共有', len(good), '筆有good的留言')
+
+bad = []
+for d in data:
+	if 'bad' in d:
+		bad.append(d)
+print('總共有', len(bad), '筆有bad的留言')
+print('good佔', (len(good)/len(data))*100, '%')
+print('bad佔', (len(bad)/len(data))*100, '%')
